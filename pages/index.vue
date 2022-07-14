9b170e4b5004335aa04fc3859ad998091f885a86
Konstantin BIFERT <hello@kissu.io>
2022-07-14 11:05:16 +0200

    working

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <Tutorial />
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  mounted () {
+    const x = { person: { real: true, name: 'bob' } }
+    if (x?.person?.real) {
+      console.log(x.person.name)
+    }
+  },
+}
+</script>
